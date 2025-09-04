205dc4bbbff20209057836c6c657651d73096cff
Kevinraj-Arul <kevinraj.arul@diggibyte.com>
2025-09-04 10:51:04 +0530

    this data type file

---
+++ src/Data type.py
@@ -0,0 +1,30 @@
+#data type 
+a=5
+print(type(a))
+
+#slicing string
+b="hello world"
+print(b[2:5])
+#slice to end
+b="hello world"
+print(b[2:])
+#uppercase
+h="hello World"
+print(h.upper())
+#Remove White space
+a=" hello world  "
+print(a.strip())
+#Replace
+a="string"
+print(a.replace("t","p"))
+#split
+a="Hello kevin"
+print(a.split(","))
+#contatenate
+a="kevin"
+b="raj"
+print(a+b)
+#f string
+age=26
+t=f"i am kevin my age is {age}"
+print(t)
