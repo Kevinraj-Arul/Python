84fd9a03fbe986ce0dd078a25bf66d73a338a70f
Kevinraj-Arul <kevinraj.arul@diggibyte.com>
2025-09-04 15:01:05 +0530

    this file is while loop contional file

--- src/loop2.py
+++ src/loop2.py
@@ -8,4 +8,14 @@ Correct_pin="123"
 enter_pin=" "
 while enter_pin!=Correct_pin:
     enter_pin =input("enter your pin:")
-print("Granted access",enter_pin)
+print("Granted access")
+#while important
+iteams=[]
+
+while True:
+    iteam =input("add iteam:")
+    if iteam.lower() == "done":
+        break
+    iteams.append(iteam)
+print ("iteams in cart",iteams)
+
