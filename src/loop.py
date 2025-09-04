01d870a3fbe14fc37a12cee14ca9c2a4e62108c5
Kevinraj-Arul <kevinraj.arul@diggibyte.com>
2025-09-04 14:24:41 +0530

    this file for loop contional file

---
+++ src/loop.py
@@ -0,0 +1,19 @@
+#for 
+for i in range(1,6):
+    print(i)
+#for variable in sequence
+l=[1,2,3,4,5]
+for i in l:
+    print (i)
+#for break 
+n=[1,2,3,4,5]
+for i in n:
+    if i ==5:
+        break
+print(i)
+#for continue
+n=[1,2,-3,3,-4,10]
+for i in n:
+    if i<0:
+        continue
+print(i)
