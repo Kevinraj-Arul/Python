0fc194e44b1b0abdc7d6d794f5478648e053152b
Kevinraj-Arul <kevinraj.arul@diggibyte.com>
2025-09-04 14:16:52 +0530

    this is control statemen

--- src/control.py
+++ src/control.py
@@ -1,4 +1,21 @@
 #if Statement
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+#Else Statement 
+a=10
+b=20
+if a>b:
+  print("a is greater than b")
+else:
+  print("b is greater than a")
+#Elif Statement
 a=20
-if a<20:
-    print("a is less than 20")
+b=20
+if a>b:
+  print("a is greater than of b")
+elif a==b:
+  print("a and b are equal")
+else:
+  print("invalid syntax")
