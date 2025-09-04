b4be9a96f958347928a629b4e4e4c6b86fc72dc2
Kevinraj-Arul <kevinraj.arul@diggibyte.com>
2025-09-04 14:56:51 +0530

    this file While& C:/Users/KevinrajArul/AppData/Local/Microsoft/WindowsApps/python3.13.exe c:/Users/KevinrajArul/Python/src/loop2.py loop contional file

---
+++ src/loop2.py
@@ -0,0 +1,11 @@
+#While
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+#while important 
+Correct_pin="123"
+enter_pin=" "
+while enter_pin!=Correct_pin:
+    enter_pin =input("enter your pin:")
+print("Granted access",enter_pin)
