2ceaabea96cdad3a51de5e72d4e5498a2c658639
Kevinraj-Arul <kevinraj.arul@diggibyte.com>
2025-09-04 23:24:31 +0530

    this file is tuple in python

---
+++ src/tuple.py
@@ -0,0 +1,23 @@
+#simple tuple
+cars=["Audi","Bmw","Benz"]
+print(tuple(cars))
+#index
+cars=["Audi","Bmw","Benz"]
+t=cars[0]
+print(t)
+
+#slicing
+cars=["Audi","Bmw","Benz"]
+t=cars[-1:]
+print(t)
+#packing and unpacking
+point=(10,20,30)
+x,y,z=point
+print(x,y,z)
+
+#built in function
+num=[5,4,8,3]
+print(len(num))
+print(max(num))
+print(min(num))
+print(sum(num))
